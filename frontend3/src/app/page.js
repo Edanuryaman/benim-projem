48408970b3ce45c04eb2ba190586c25a4e78198c
edanur.yaman <edanur.yaman1268@gmail.com>
2025-12-09 17:13:18 +0300

    next.js projesi oluşturuldu

---
+++ frontend3/src/app/page.js
@@ -0,0 +1,11 @@
+'use client';
+import { Button } from 'primereact/button';
+
+export default function HomePage() {
+    return (
+        <div style={{ padding: '2rem' }}>
+            <h1>Welcome to Frontend2 Starter</h1>
+            <Button label="Tıkla!" icon="pi pi-check" />
+        </div>
+    );
+}
